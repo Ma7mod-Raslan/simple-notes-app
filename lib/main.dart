cd45a7c12b970001945bb47aee3251f05af6f81a
Ma7mod-Raslan <ma7mod-raslan@outlook.com>
2025-05-05 02:32:37 +0300

    ubdated main.dart file after added Iman's change

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/note.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(NoteAdapter());
   runApp(const SimpleNotesApp());
 }
 
